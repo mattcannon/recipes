2133d251abd498fabd58f47e6d4a11d37ab86a6b
mattcannon <matt@mattcannon.uk>
2018-10-29 23:06:16 +0000

    Add parsedown

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,10 @@
+@inject('md','Parsedown')
 <h1>Recent Recipes</h1>
 <div class="recipes">
 @foreach($recipes as $recipe)
     <article class="recipe">
         <header><h1>{{$recipe->title}}</h1></header>
-        {!! $recipe->steps !!}
+        {!! $md->text($recipe->steps) !!}
     </article>
 @endforeach
 </div>
